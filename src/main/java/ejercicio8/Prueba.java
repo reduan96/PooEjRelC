eb16e47a8b8a50ab9ccdd25568c9804d8aac17d3
Reduan <redu1996@gmail.com>
2020-12-01 14:35:31 +0100

    correcion final

--- src/main/java/ejercicio8/Prueba.java
+++ src/main/java/ejercicio8/Prueba.java
@@ -13,27 +13,6 @@ public class Prueba {
 
     public static void main(String[] args) {
 
-//        Cafetera cafetera1 = new Cafetera(1000.0, 200.0);
-//        System.out.println("La cafetera creada tiene " + cafetera1.getCapacidadMaxima()
-//                + " cc de capacidad máxima y tiene " + cafetera1.getCantidadActual()
-//                + " cc de capacidad actual");
-//
-//        cafetera1.llenarCafetera();
-//        System.out.println("la cafetera tiene " + cafetera1.getCantidadActual() + " cc"
-//                + " de capacidad actual");
-//
-//        cafetera1.servirTaza(900);
-//        System.out.println("la cafetera tiene " + cafetera1.getCantidadActual() + " cc"
-//                + " de capacidad actual");
-//
-//        cafetera1.agregarCafe(370);
-//        System.out.println("la cafetera tiene " + cafetera1.getCantidadActual() + " cc"
-//                + " de capacidad actual");
-//
-//        cafetera1.vaciarCafetera();
-//        System.out.println("la cafetera tiene " + cafetera1.getCantidadActual() + " cc"
-//                + " de capacidad actual");
-
         Cafetera cafetera1 = new Cafetera();
         System.out.println(cafetera1);
         
